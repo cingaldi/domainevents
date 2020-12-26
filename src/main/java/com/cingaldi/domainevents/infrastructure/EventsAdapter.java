df7d1069b4cab1c0ae7c14ac3d9c937edef078b3
cingaldi <carmine.ingaldi@glovoapp.com>
2020-12-26 23:04:13 +0100

    using jackson for serialization

--- src/main/java/com/cingaldi/domainevents/infrastructure/EventsAdapter.java
+++ src/main/java/com/cingaldi/domainevents/infrastructure/EventsAdapter.java
@@ -1,27 +1,37 @@
 package com.cingaldi.domainevents.infrastructure;
 
 import com.cingaldi.commons.domaintools.CrossDomainEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import javax.transaction.Transactional;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Component
+@Slf4j
 public class EventsAdapter {
-
-    private static Logger log = getLogger("EventsAdapter");
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    @Async
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @EventListener
-    public void onCrossDomainEvent(CrossDomainEvent evt) {
-        log.info("publishing event of type {} with routing key {}" , evt.eventType(), evt.topic());
+    public void onCrossDomainEvent(CrossDomainEvent evt) throws JsonProcessingException {
+
+        String message = objectMapper.writeValueAsString(evt);
+        log.info("publishing event => payload={}" , message);
+
         rabbitTemplate.convertAndSend(evt.topic(), evt.serialize());
     }
 }

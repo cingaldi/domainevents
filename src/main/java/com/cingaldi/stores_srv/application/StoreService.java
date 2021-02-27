9baadf7f713347fce8f342e2ce6260cf5eeac06a
cingaldi <carmine.ingaldi@glovoapp.com>
2021-02-27 14:58:00 +0100

    wip introducing order creation api

--- src/main/java/com/cingaldi/stores_srv/application/StoreService.java
+++ src/main/java/com/cingaldi/stores_srv/application/StoreService.java
@@ -1,7 +1,11 @@
 package com.cingaldi.stores_srv.application;
 
 import com.cingaldi.commons.domaintools.DomainCollectionResult;
+import com.cingaldi.commons.domaintools.DomainEntityResult;
 import com.cingaldi.orders_srv.domain.events.OrderCreatedEvt;
+import com.cingaldi.stores_srv.application.dtos.CreateStoreDTO;
+import com.cingaldi.stores_srv.application.dtos.CreateStoreOldDTO;
+import com.cingaldi.stores_srv.domain.models.Store;
 import com.cingaldi.stores_srv.domain.models.StoreOrder;
 import com.cingaldi.stores_srv.domain.repositories.StoreOrdersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +25,14 @@ public class StoreService {
     @Async
     public void onOrderCreated(OrderCreatedEvt evt) {
         repository.save(new StoreOrder(evt.getAggregateId(), evt.getStoreId()));
+    }
+
+    public DomainEntityResult<Store> createStore(CreateStoreOldDTO createStore) {
+        return null;
+    }
+
+    public DomainEntityResult<Store> createStore(CreateStoreDTO createStore) {
+        return null;
     }
 
     public DomainCollectionResult<StoreOrder> getOrdersFromStore(Long storeId) {
--- src/main/java/com/cingaldi/stores_srv/presentation/StoreOrdersController.java
+++ src/main/java/com/cingaldi/stores_srv/presentation/StoreOrdersController.java
@@ -1,14 +1,19 @@
 package com.cingaldi.stores_srv.presentation;
 
 import com.cingaldi.commons.domaintools.DomainCollectionResult;
+import com.cingaldi.commons.domaintools.DomainEntityResult;
 import com.cingaldi.commons.resttools.BodyVersionSwitch;
 import com.cingaldi.commons.resttools.CollectionResource;
 import com.cingaldi.commons.resttools.ErrorResource;
 import com.cingaldi.commons.resttools.exceptions.MediaNotSupportedException;
 import com.cingaldi.stores_srv.application.StoreService;
+import com.cingaldi.stores_srv.application.dtos.CreateStoreDTO;
+import com.cingaldi.stores_srv.application.dtos.CreateStoreOldDTO;
+import com.cingaldi.stores_srv.domain.models.Store;
 import com.cingaldi.stores_srv.domain.models.StoreOrder;
 import com.cingaldi.stores_srv.presentation.vm.StoreOrderVM;
 import com.cingaldi.stores_srv.presentation.vm.StoreOrderOldVM;
+import com.cingaldi.stores_srv.presentation.vm.StoreVM;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +25,22 @@ public class StoreOrdersController {
 
     @Autowired
     private StoreService storeService;
+
+    @PostMapping("/stores")
+    public ResponseEntity createStore(@RequestBody Object body, @RequestHeader("Content-Type") String contentType) {
+
+        return new BodyVersionSwitch()
+                .withVersion(contentType, "")
+                .whenDefault(()->{
+                    StoreVM result = storeService.createStore((CreateStoreOldDTO)body).mapTo(StoreVM::fromEntity);
+                    return new ResponseEntity(result, HttpStatus.CREATED);
+                })
+                .orMatch("v1", ()->{
+                    StoreVM result = storeService.createStore((CreateStoreDTO) body).mapTo(StoreVM::fromEntity);
+                    return new ResponseEntity(result, HttpStatus.CREATED);
+                })
+                .getResult();
+    }
 
     @GetMapping("/stores/{storeId}/orders")
     public ResponseEntity getOrders(@PathVariable("storeId") Long storeId, @RequestHeader("Accept") String acceptHeader) {
